87fcda3fc4e9256ce6ef5af213a3885180a04cc6
Zhj <1342497867@qq.com>
2022-07-05 20:12:08 +0800

    hot-fix commit2

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -15,6 +15,7 @@ public class UserController {
      public String save(Book book){
          System.out.println("book save ==>"+book);
          System.out.println("book2 save ==>"+book);
+         System.out.println("book3 save ==>"+book);
          return "{'module':'booksave success'}";
      }
 }
